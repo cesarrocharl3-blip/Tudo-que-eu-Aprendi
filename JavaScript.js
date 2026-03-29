1c3ebce17f59ca468543512d8d9aecf3cf75337b
cesarrocharl3-blip <cesarrocharl3@gmail.com>
2026-03-29 00:41:28 -0300

    Parte do JScript já em produção

---
+++ JavaScript.js
@@ -0,0 +1,39 @@
+
+
+/* Abrir console no VSCode: Ctrl + Alt + N */
+
+console.log("Olá, mundo!"); //Código para escrever algo no console//
+
+//Variáveis//
+
+let variavel1 = 'Pode alterar'
+const variavel2 = 'Não pode alterar (MAIS RECOMENDADA A SE USAR)'
+var variavel3 = 'Quando a variável não esta mais em uso'
+
+console.log(variavel1);
+console.log(variavel2);
+console.log(variavel3);
+
+//Strings//
+
+const tx1 = "Texto com aspas duplas"
+const tx2 = 'Texto com aspas simples'
+const tx3 = `Texto com crase` //Template String ou Template Literals//
+
+console.log(tx1);
+console.log(tx2);
+console.log(tx3);
+
+//Template Strings//
+
+const idade = 28
+const carro = "Fusca"
+
+console.log(`Eu tenho ${idade} anos e meu carro é um ${carro}, estamos no ano de ${2000 + 26}.`);  //Com as template strings, é possível fazer contas dentro do ${}//
+
+// Numbers //
+
+const idade2 = "28" 
+const conta = 30 + 20 
+
+console.log(`Minha idade ${idade2} e ${conta}`) 
