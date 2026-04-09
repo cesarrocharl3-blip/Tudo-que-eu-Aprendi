c60c34286afe6bb3ed0487b81f48314b0b266021
cesarrocharl3-blip <cesarrocharl3@gmail.com>
2026-04-09 14:20:07 -0300

    ok

--- JavaScript.js
+++ JavaScript.js
@@ -116,15 +116,17 @@ if (notaAluno1 == notaDeCorte) {
 
 // Eventos //
 
-function EventoClickB1() {
+function eventoClickB1() {
 
     alert("Você clicou no botão 1!");
 }   
 
 // onkeypress - Pedi para a IA me ajudar a colocar um contador //
 
-const input = document.querySelector(".input-eventos")  //Está pegando o elemento HTML que tem a class input-eventos e guardando ele na variável input para poder usar depois.//
+const input = document.querySelector(".inputEventos")  //Está pegando o elemento HTML que tem a class inputEventos e guardando ele na variável input para poder usar depois.//
+
 const contador = document.getElementById("contador")    //Está pegando o elemento HTML que tem o id contador (o <span> abaixo do button do HTML) e guardando na variável contador.//
+
 let teclas = 0                                          //Está criando uma variável chamada "teclas" começando com o valor zero, ela vai ser usada para contar as teclas.//
 
 input.onkeypress = function() {                         //Está dizendo: "quando uma tecla for pressionada dentro do input, execute essa função".//
@@ -132,3 +134,52 @@ input.onkeypress = function() {                         //Está dizendo: "quando
     contador.textContent = teclas + " teclas"           //Está atualizando o texto do <span> com o valor atual de teclas. Por exemplo, se pressionou 3 teclas, vai mostrar "3 teclas" na tela.//
     console.log("Você digitou algo no input!");         //Mostra a mensagem no console do navegador toda vez que uma tecla for pressionada.//
 }
+
+//Coloca no console o que foi digitado no input //
+const input2 = document.querySelector(".inputEventos2")
+
+function digiteiNoInput2() {
+
+    console.log(input2.value)
+}
+
+// Faz um alert com o que foi digitado no input //
+const input3 = document.querySelector(".inputEventos3")
+
+function eventoClickB2() {
+    
+    alert(input3.value)
+}   
+
+// Aparecer em baixo o que foi digitado no input 1 //
+const paragrafo = document.getElementById("p-eventos")
+function eventoClickB3() {
+
+    paragrafo.textContent=(input.value) 
+}
+    
+// Add Event Listener //
+
+const inputAel = document.querySelector(".inputAel")
+const select = document.querySelector(".selectAel")
+const buttonAel = document.querySelector(".buttonAel")
+
+select. addEventListener("change", function() {      // Está dizendo: "quando o select for alterado, execute essa função"( Essa é uma função anônima, sem nome).//
+
+    console.log("O select foi alterado!")           //Mostra a mensagem no console do navegador toda vez que o select for alterado.//
+})
+
+//Outra forma de fazer a mesma coisa://
+
+function valorAlterado() {
+    console.log("O select foi alterado! 2ª forma")
+}
+
+select. addEventListener("change", valorAlterado)
+
+//Para pegar informações do evento coloca qualquer palavra no parentêses do "function" e console.log (event) //
+function valorAlterado(event) {
+    console.log(event)
+}
+
+select. addEventListener("change", valorAlterado)
